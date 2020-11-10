d6023f5c383e1988107d9ea83d1246342a026681
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2020-11-10 22:55:53 +0200

    Add files via upload

---
+++ PrintNumbersInReverseOrder__02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrintNumbersInReverseOrder__02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String [] numbers = new String[n];
+
+
+        for (int i = 0; i < n ; i++) {
+            numbers [i] = scanner.nextLine();
+
+        }
+        for (int i = numbers.length-1; i >= 0 ; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+
+
+
+    }
+}
